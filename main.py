33c4d566eebec8174133ab7e619856d774bdc5e4
mmuehlemann <74836309+mmuehlemann@users.noreply.github.com>
2021-02-16 11:23:25 +0100

    Update main.py
    Updated file

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import csv
-from ipaddress import ip_address, ip_interface, ip_network
+from ipaddress import ip_address, ip_network
+from operator import itemgetter
+from datetime import date
 from pathlib import Path
 import argparse
+import urllib.request
+import csv
 import re
+
 
 # Define Argument Parser for script file arguments
 parser = argparse.ArgumentParser(description="CSV Network Finder", formatter_class=argparse.RawTextHelpFormatter)
@@ -10,56 +14,66 @@ parser.add_argument("--ip", help="IP Address to check for")
 args = parser.parse_args()
 
 # Define CSV file location
-csvpath = Path(r"C:\Users\BGP_TABLE_20210212.CSV")
+today = date.today()
+csvurl = "http://10.0.0.1/FUSION_RT/FUSION_BGP_TABLE_"+ today.strftime("%Y%m%d") + ".CSV"
+csvfilename = "FUSION_BGP_TABLE_" + today.strftime("%Y%m%d") + ".CSV"
+csvpath = urllib.request.urlretrieve(csvurl, filename=csvfilename)
+#csvpath = requests.get("http://10.0.0.1/FUSION_RT/FUSION_BGP_TABLE_" + today.strftime("%Y%m%d") + ".CSV")
 
-# Define global variables
-csvreader = ""
-nwlist = []
-lookupip = args.ip.split(",")
-resultsdict = {}
 
-def readcsv():
-    # Read CSV and add content to a variable
+def readcsv(csvpath):
+    # Read CSV and convert list of lists to dictionary
     with open(csvpath, "r") as csvfile:
         csvreader = csv.reader(csvfile, delimiter=';')
-        for line in csvreader:
-            nwlist.append(line)
-    for index, line in enumerate(nwlist):
-        nwlist[index][1] = re.sub("[\[\]]","", re.sub("\:", " ", line[1])).lstrip().rstrip()
+        bgpdict = {ip_network(x[0]): re.sub("[\[\]]", "", re.sub("\:", " ", x[1])).lstrip().rstrip() for x in csvreader}
+    return bgpdict
 
 
-def inputvalidation():
+def inputvalidation(argumentstring):
     # Validate entered IP address
-    for ip in lookupip:
+    ip_list = []
+    for ip in argumentstring.split(','):
         try:
-            ip_address(ip)
-            lookupip[lookupip.index(ip)] = ip_address(ip)
-            # legal
+            ip_list.append(ip_address(ip))
         except:
-            print("{ip} Not a valid IP address, try again".format(ip=ip))
+            print("{ip} Not a valid IP address, ignoring...\n======================".format(ip=ip))
+    return ip_list
 
 
-def networklookup():
-    readcsv()
-    inputvalidation()
-    # Convert Strings in nwlist to network objects
-    for row in nwlist: row[0] = ip_network(row[0])
-    # Do the actual crosscheck
-    for address in lookupip:
-        resultsdict[address] = []
-        for nw in nwlist:
-            if address in nw[0]:
-                resultsdict[address].append(nw)
+def networklookup(bgpdict, lookupip):
+    output = []
+    resultsdict = {}
     
-    if resultsdict != {}:
-        for key, value in resultsdict.items():
-            print("{ip} is in this VRF:".format(ip=key))
-            print(" ")
-            for entry in value:
-                print("VRF {vrf}, subnet {nw}".format(vrf=entry[1], nw=entry[0]))
-            print("======================")
-    else:
-        for ip in lookupip:
-            print("{ip} is not in the BGP table".format(ip=ip))
+    # Do the actual crosscheck
+    for ip in lookupip:
+        resultsdict[ip] = []
+        for network, name in bgpdict.items():
+            if ip in network:
+                resultsdict[ip].append((name, network))
 
-networklookup()
+    output.append("============================================")
+    output.append("Most exact matches for your addresses:")
+    output.append("============================================")
+
+    for key in resultsdict:
+        most_exact = max(resultsdict[key], key=itemgetter(1))[0]
+        output.append("The most exact VRF for " + str(key) + " is " + most_exact)
+
+    output.append(" ")
+    output.append("============================================")
+    output.append("List of all matches:")
+    output.append("============================================")
+
+    for key, value in resultsdict.items():
+        if len(value) > 0:
+            output.append("{ip} is in this VRF:".format(ip=key))
+            for name, network in value:
+                output.append("      VRF {vrf}, subnet {nw}".format(vrf=name, nw=network))
+            output.append("---------------------")
+        else:
+            output.append("{ip} is not in the BGP table".format(ip=ip))
+ 
+    return output
+
+
+print('\n'.join(networklookup(readcsv(csvpath), inputvalidation(args.ip))))
